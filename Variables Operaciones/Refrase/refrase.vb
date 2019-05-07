2c1f6915fb15a80a80dde891c838cfe9ae6789d3
GonzLeandro <joayugi@gmail.com>
2019-05-06 21:36:30 -0300

    ya me canse de actualizar

--- Variables Operaciones/Refrase/refrase.vb
+++ Variables Operaciones/Refrase/refrase.vb
@@ -4,14 +4,14 @@
 
         Dim Palabra1, Palabra2 As String
         Dim frase As String = "Aunque la cadena sea de oro, sirve para lo mismo"
+        Console.WriteLine(frase)
 
         Console.Write("Ingrese una palabra: ")
         Palabra1 = Console.ReadLine
         Console.Write("Ingrese otra palabra: ")
         Palabra2 = Console.ReadLine
 
-        Console.WriteLine(Replace(frase, "la", Palabra1))
-        Console.WriteLine(Replace(frase, "cadena", Palabra2))
+        Console.WriteLine(Replace(frase, Palabra1, Palabra2))
 
 
 
