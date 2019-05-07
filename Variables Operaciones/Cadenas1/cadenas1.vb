69bdb3fb1bfb002ffd3259a81c9ba657aa1ab95d
GonzLeandro <joayugi@gmail.com>
2019-05-06 21:29:15 -0300

    ok

---
+++ Variables Operaciones/Cadenas1/cadenas1.vb
@@ -0,0 +1,15 @@
+﻿Module cadenas1
+
+    Sub Main()
+        Dim cadena As String
+        Console.Write("Ingrese una palabra: ")
+        cadena = Console.ReadLine
+
+        Console.WriteLine("La primer 'a' aparece en la pocision : " & InStr(cadena, "a"))
+        Console.WriteLine("La primer 'o' aparece en la pocision : " & InStr(cadena, "o"))
+        Console.WriteLine("La cadena invertida es: " & StrReverse(cadena))
+        Console.ReadKey()
+
+
+    End Sub
+End Module
---
+++ Variables Operaciones/Refrase/refrase.vb
@@ -0,0 +1,23 @@
+﻿Module refrase
+
+    Sub Main()
+
+        Dim Palabra1, Palabra2 As String
+        Dim frase As String = "Aunque la cadena sea de oro, sirve para lo mismo"
+
+        Console.Write("Ingrese una palabra: ")
+        Palabra1 = Console.ReadLine
+        Console.Write("Ingrese otra palabra: ")
+        Palabra2 = Console.ReadLine
+
+        Console.WriteLine(Replace(frase, "la", Palabra1))
+        Console.WriteLine(Replace(frase, "cadena", Palabra2))
+
+
+
+
+
+
+    End Sub
+
+End Module
