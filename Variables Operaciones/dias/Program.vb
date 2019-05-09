0b2fcc02b7d31bcbe1e436a6b2f1b7639679cfa2
GonzLeandro <joayugi@gmail.com>
2019-05-08 21:56:06 -0300

    1.19 listo

---
+++ Variables Operaciones/dias/Program.vb
@@ -0,0 +1,23 @@
+Imports System
+
+Enum diasdelasemana
+    domingo = 1
+    lunes
+    martes
+    miercoles
+    jueves
+    viernes
+    sabado
+End Enum
+
+Module Program
+    Sub Main(args As String())
+        Dim dia As diasdelasemana
+        Console.WriteLine("Ingrese un numero del 1 al 7: ")
+        dia = Console.ReadLine
+
+        Console.WriteLine("El dia ingresado es: " & dia.ToString)
+
+
+    End Sub
+End Module
